ac92cb658496659accf4898c27d1859d213a4e0a
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-03-23 22:22:29 -0400

    added shell cmds, but facing issues with volume creation

--- HW/HW2 Codebase/boxing/run_docker.sh
+++ HW/HW2 Codebase/boxing/run_docker.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Variables
-IMAGE_NAME=
-CONTAINER_TAG=
-HOST_PORT=5000
-CONTAINER_PORT=5000
-DB_VOLUME_PATH=  # Adjust this to the desired host path for the database persistence
-BUILD=  # Set this to true if you want to build the image
+IMAGE_NAME=hw2-image
+CONTAINER_TAG=1.0
+HOST_PORT=5002
+CONTAINER_PORT=5002
+DB_VOLUME_PATH=my_volume # Adjust this to the desired host path for the database persistence
+BUILD=true # Set this to true if you want to build the image
 
 # Check if we need to build the Docker image
 if [ "$BUILD" = true ]; then
   echo "Building Docker image..."
+
+  docker build -t ${IMAGE_NAME}:${CONTAINER_TAG} .
 
 else
   echo "Skipping Docker image build..."
@@ -20,16 +22,20 @@ fi
 if [ ! -d "${DB_VOLUME_PATH}" ]; then
   echo "Creating database directory at ${DB_VOLUME_PATH}..."
 
+  docker volume create ${DB_VOLUME_PATH}
 fi
 
 # Stop and remove the running container if it exists
 if [ "$(docker ps -q -a -f name=${IMAGE_NAME}_container)" ]; then
     echo "Stopping running container: ${IMAGE_NAME}_container"
 
+    docker stop ${IMAGE_NAME}_container
 
     # Check if the stop was successful
     if [ $? -eq 0 ]; then
         echo "Removing container: ${IMAGE_NAME}_container"
+
+        docker rm ${IMAGE_NAME}_container
 
     else
         echo "Failed to stop container: ${IMAGE_NAME}_container"
@@ -41,5 +47,11 @@ fi
 
 # Run the Docker container with the necessary ports and volume mappings
 echo "Running Docker container..."
+
+docker run -d \
+-p ${HOST_PORT}:${CONTAINER_PORT} \
+-v ${DB_VOLUME_PATH}:/app/data \
+--name ${IMAGE_NAME}_container \
+${IMAGE_NAME}:${CONTAINER_TAG}
 
 echo "Docker container is running on port ${HOST_PORT}."
