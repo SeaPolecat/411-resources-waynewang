ada138934d28cd963418d89e3ae40ea9e53b4879
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-03-30 20:34:58 -0400

    add rest of the fixtures and finish testing get_boxers

--- HW/HW2 Codebase/boxing/tests/test_ring_model.py
+++ HW/HW2 Codebase/boxing/tests/test_ring_model.py
@@ -13,30 +13,27 @@ from boxing.models.boxers_model import Boxer
 
 @pytest.fixture()
 def ring_model():
-    """Fixture to provide a new instance of PlaylistModel for each test."""
+    """Fixture to provide a new instance of RingModel for each test."""
     return RingModel()
 
-'''
 @pytest.fixture
-def mock_update_play_count(mocker):
-    """Mock the update_play_count function for testing purposes."""
-    return mocker.patch("playlist.models.playlist_model.update_play_count")
-'''
+def mock_update_boxer_stats(mocker):
+    """Mock the update_boxer_stats function for testing purposes."""
+    return mocker.patch("boxer.models.boxer_model.update_boxer_stats")
 
-"""Fixtures providing sample songs for the tests."""
+
+"""Fixtures providing sample boxers for the tests."""
 @pytest.fixture
 def sample_boxer1():
     return Boxer(1, "Wayne", 200, 100, 5, 20)
 
 @pytest.fixture
 def sample_boxer2():
-    return Boxer(1, "Wayne", 200, 100, 5, 20)
+    return Boxer(2, "Machamp", 300, 280, 8, 25)
 
-'''
 @pytest.fixture
-def sample_playlist(sample_song1, sample_song2):
-    return [sample_song1, sample_song2]'
-'''
+def sample_ring(sample_boxer1, sample_boxer2):
+    return [sample_boxer1, sample_boxer2]
 
 
 ######################################################
@@ -54,3 +51,22 @@ def test_clear_ring(ring_model, sample_boxer1):
 
     ring_model.clear_ring()
     assert len(ring_model.ring) == 0, "Ring should be empty after clearing"
+
+
+######################################################
+#
+#    get_boxers
+#
+######################################################
+
+
+def test_get_boxers(ring_model, sample_ring):
+    """
+    Test successfully retrieving boxers from the ring.
+    """
+    ring_model.ring.extend(sample_ring)
+
+    boxers = ring_model.get_boxers()
+    assert len(boxers) == 2
+    assert boxers[0].id == 1
+    assert boxers[1].id == 2
